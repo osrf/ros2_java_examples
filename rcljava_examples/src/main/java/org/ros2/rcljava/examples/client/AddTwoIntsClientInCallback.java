7bad48438a213fcd39ee0d5325b50777c894da9a
Jacob Perron <jacob@openrobotics.org>
2021-11-19 18:00:42 -0800

    Add example of calling service client inside a callback
    Signed-off-by: Jacob Perron <jacob@openrobotics.org>

---
+++ rcljava_examples/src/main/java/org/ros2/rcljava/examples/client/AddTwoIntsClientInCallback.java
@@ -0,0 +1,81 @@
+/* Copyright 2016-2017 Esteve Fernandez <esteve@apache.org>
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.ros2.rcljava.examples.client;
+
+import org.ros2.rcljava.RCLJava;
+import org.ros2.rcljava.client.Client;
+import org.ros2.rcljava.executors.MultiThreadedExecutor;
+import org.ros2.rcljava.node.Node;
+import org.ros2.rcljava.node.ComposableNode;
+import org.ros2.rcljava.consumers.Consumer;
+import org.ros2.rcljava.subscription.Subscription;
+
+import java.util.concurrent.Future;
+
+public class AddTwoIntsClientInCallback {
+
+  private static Client<example_interfaces.srv.AddTwoInts> client;
+
+  public static void topicCallback(final std_msgs.msg.String msg) {
+    System.out.println("I heard: [" + msg.getData() + "]");
+
+    example_interfaces.srv.AddTwoInts_Request request =
+        new example_interfaces.srv.AddTwoInts_Request();
+    request.setA(2);
+    request.setB(3);
+
+    try {
+      if (client.waitForService()) {
+        System.out.println("Service is ready");
+        Future<example_interfaces.srv.AddTwoInts_Response> future =
+            client.asyncSendRequest(request);
+        System.out.println("Waiting for response");
+        System.out.println(
+            "result of " + request.getA() + " + " + request.getB() + " = " + future.get().getSum());
+      }
+    } catch (Exception e) {
+      System.out.println("Exception in callback: " + e.getMessage());
+    }
+
+  }
+
+  public static void main(final String[] args) throws InterruptedException, Exception {
+    // Initialize RCL
+    RCLJava.rclJavaInit();
+
+    // Let's create a new Node
+    Node node = RCLJava.createNode("minimal_client");
+
+    client = node.<example_interfaces.srv.AddTwoInts>createClient(
+      example_interfaces.srv.AddTwoInts.class, "add_two_ints");
+
+    Subscription<std_msgs.msg.String> subscription = node.<std_msgs.msg.String>createSubscription(
+      std_msgs.msg.String.class, "topic", AddTwoIntsClientInCallback::topicCallback);
+
+    // SingleThreadedExecutor does not work
+    // RCLJava.spin(node);
+
+    ComposableNode composableNode = new ComposableNode() {
+      public Node getNode() {
+        return node;
+      }
+    };
+
+    MultiThreadedExecutor executor = new MultiThreadedExecutor();
+    executor.addNode(composableNode);
+    executor.spin();
+  }
+}
